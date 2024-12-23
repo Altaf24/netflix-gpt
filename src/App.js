afe2f99d1c699a5bcd7b763e56065ec1e577c8b4
Altaf24 <tambolialtaf1419@gmail.com>
2024-12-23 15:26:10 +0530

    your commit message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-green-600">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
